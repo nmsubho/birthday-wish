b0066309119f14f0923c8fa05fa86a7c242f03dd
subhousa <subho@dynamicsoft.us>
2024-04-25 16:09:30 +0600

    added a corn job that will be executed everyday at mid night

--- app.js
+++ app.js
@@ -6,12 +6,48 @@ const cors = require("cors");
 const httpStatus = require("http-status");
 const globalErrorHandler = require("./src/middlewares/globalErrorHandler");
 const routes = require("./src/routes");
+const Customer = require("./src/models/customer.model");
+const ApiError = require("./src/errors/ApiError");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/", routes);
+
+// Schedule email sending on birthdays
+cron.schedule("0 0 * * *", async () => {
+  const today = new Date();
+  const currentMonth = today.getMonth() + 1;
+  const currentDate = today.getDate();
+
+  try {
+    const customers = await Customer.aggregate([
+      {
+        $addFields: {
+          birthMonth: { $month: "$birthdate" },
+          birthDate: { $dayOfMonth: "$birthdate" },
+        },
+      },
+      {
+        $match: {
+          $expr: {
+            $and: [
+              { $eq: ["$birthMonth", currentMonth] },
+              { $eq: ["$birthDate", currentDate] },
+            ],
+          },
+        },
+      },
+    ]);
+
+    // customers.forEach(customer => {
+    //   sendBirthdayEmail(customer.email, customer.name);
+    // });
+  } catch (error) {
+    throw new ApiError(error);
+  }
+});
 
 app.use(globalErrorHandler);
 
@@ -25,15 +61,5 @@ app.use((req, res, next) => {
   });
   next();
 });
-
-// // Schedule email sending on birthdays
-// cron.schedule("0 0 * * *", async () => {
-//   const today = new Date();
-//   const users = await customerCollection.find({}).toArray();
-//   console.log(users);
-//   // users.forEach(user => {
-//   //   sendBirthdayEmail(user.email, user.name);
-//   // });
-// });
 
 module.exports = app;
